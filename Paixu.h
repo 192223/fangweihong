1ebbbc94616ea58c96bab9324a96019937e70df9
192223 <74724875+192223@users.noreply.github.com>
2020-11-26 22:40:34 +0800

    Add files via upload

---
+++ Paixu.h
@@ -0,0 +1,12 @@
+#ifndef Paixu_h
+#define Paixu_h
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void Bubble(int a[], int n);
+void Insert(int a[], int n);
+void Select(int a[], int n);
+void Quick(int a[], int n);
+
+#endif
